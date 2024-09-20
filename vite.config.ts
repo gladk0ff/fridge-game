90555150663288998029ffd1b4c5ec8383f9165d
gladk0ff <sergladkov@outlook.com>
2024-09-20 03:17:48 +0300

    test build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fridge-game/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
